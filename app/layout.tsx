1718bd8244bd5a5d4cec20e6dc3d63ef84282561
So Fujishima <s26fuji@gmail.com>
2025-03-09 22:07:09 +0900

    ファビコンの設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "resale-paco",
   description: "せどり必須ツール",
+  icons: [
+    { rel: 'icon', url: '/favicon.svg', type: 'image/svg+xml' },
+  ],
 };
 
 export default function RootLayout({
